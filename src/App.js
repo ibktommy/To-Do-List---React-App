80133c33c400e243b786c9d3584f22c38909bf84
ibktommy <atomquartz@gmail.com>
2022-07-14 14:48:28 +0100

    Setting App State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import List from './components/List.jsx'
+import Alert from './components/Alert.jsx'
+
 function App() {
+  // Setting App States
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
   return (
     <>
-      <h1>To Do App</h1>
+      <main className="container">
+        <div className="list-container">
+          <List />
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const List = () => {
+	return <div>List Component</div>;
+};
+
+export default List;
